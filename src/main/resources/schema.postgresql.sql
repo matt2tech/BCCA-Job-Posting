369599368725c7065fef65db52bdb8c247b55c38
matt2tech <mlipsey@basecampcodingacademy.org>
2019-03-07 16:18:14 -0600

    Designed job post page

--- src/main/resources/schema.postgresql.sql
+++ src/main/resources/schema.postgresql.sql
@@ -1,12 +1,14 @@
 CREATE TABLE IF NOT EXISTS postings (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    company TEXT,
-    position TEXT,
-    location TEXT,
-    desc TEXT,
-    exp TEXT,
-    employment TEXT,
+    name TEXT NOT NULL,
+    company TEXT NOT NULL,
+    position TEXT NOT NULL,
+    location TEXT NOT NULL,
+    desc TEXT NOT NULL,
+    exp TEXT NOT NULL,
+    industry TEXT NOT NULL,
     benefits TEXT,
     logo URL,
+    qualifications TEXT,
+
 )
