c7b427b85fb7a12a974e9e395cd1040bb6edc622
matt2tech <mlipsey@basecampcodingacademy.org>
2019-03-14 10:07:04 -0500

    Jobs can now be viewed in greater detail with job pages

--- src/main/java/com/bcca/app/controllers/HomeController.java
+++ src/main/java/com/bcca/app/controllers/HomeController.java
@@ -6,6 +6,7 @@ import com.bcca.app.repositories.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
@@ -24,4 +25,17 @@ public class HomeController {
         model.addAttribute("jobs", jobRepository.findAll());
         return "home";
     }
+
+    @GetMapping("/{id}")
+    public String getJobPage(Model model, @PathVariable(value = "id") Integer id) {
+        var job = jobRepository.findById(id);
+        if(job.isPresent()) {
+            model.addAttribute("job", job.get());
+            return "job";
+        } else {
+            return "404";
+        }
+    }
+
+
 }
